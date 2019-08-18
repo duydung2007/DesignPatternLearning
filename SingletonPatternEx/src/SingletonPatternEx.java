b586aa76bac570f10de6294c95e60f181d509e01
Duy Dung <duydung2007@gmail.com>
2019-08-18 17:54:07 +0700

    ! Use Eager initialization” to replace “Lazy initialization

--- SingletonPatternEx/src/SingletonPatternEx.java
+++ SingletonPatternEx/src/SingletonPatternEx.java
@@ -1,17 +1,10 @@
 class MakeACaptain {
-	private static MakeACaptain _captain;
+	private static MakeACaptain _captain = new MakeACaptain();
 
 	private MakeACaptain() {
 	}
 
 	public static MakeACaptain getCaptain() {
-		if (_captain == null) {
-			_captain = new MakeACaptain();
-			System.out.println("New captain selected for our team");
-		} else {
-			System.out.print("You already have a Captain for your team.");
-			System.out.println("Send him for the toss.");
-		}
 		return _captain;
 	}
 }
