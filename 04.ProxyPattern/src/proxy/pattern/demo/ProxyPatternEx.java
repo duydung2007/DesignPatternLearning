eba268048e57656872d19ffe46cd05f6e2dc3b11
Duy Dung <duydung2007@gmail.com>
2020-06-14 11:24:47 +0700

    - Implement Proxy Pattern

---
+++ 04.ProxyPattern/src/proxy/pattern/demo/ProxyPatternEx.java
@@ -0,0 +1,11 @@
+package proxy.pattern.demo;
+
+import ProxyClasses.Proxy;
+
+public class ProxyPatternEx {
+	public static void main(String[] args) {
+		System.out.println("***Proxy Pattern Demo***\n");
+		Proxy px = new Proxy();
+		px.doSomeWork();
+	}
+}
